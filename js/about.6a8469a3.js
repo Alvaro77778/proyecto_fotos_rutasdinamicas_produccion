397451a61ba5fbc0df1e5ef7ce69a0962c5cecdc
Alvaro77778 <bounce15_vito@hotmail.com>
2023-07-10 20:30:32 -0400

    proyecto desplegado

---
+++ js/about.6a8469a3.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkproyecto_fotos_rutasdinamicas"]=self["webpackChunkproyecto_fotos_rutasdinamicas"]||[]).push([[443],{782:function(t,a,n){n.r(a),n.d(a,{default:function(){return d}});var s=n(252);const o={class:"about"},r=(0,s._)("h1",null,"This is an about page",-1),u=[r];function c(t,a){return(0,s.wg)(),(0,s.iD)("div",o,u)}var e=n(744);const i={},f=(0,e.Z)(i,[["render",c]]);var d=f}}]);
+//# sourceMappingURL=about.6a8469a3.js.map
